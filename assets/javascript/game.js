744f65d02ae03cacbfcdcd8e37f48dd78fb4ed42
Britney Ross <britneyross25@yahoo.com>
2020-02-21 20:53:17 -0600

    added javascript to make game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,48 @@ var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar = [];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Computer to pick random value from alphabet array
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(computerChoice)
+
+//resets game
+var restart = function() {
+    guessesLeft = 9;
+    guessesSoFar = [];
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+
+function countGuessesLeft() {
+    document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+    }
+      
+function farUserChoice() {
+    document.getElementById('guessesSoFar').innerHTML = "Your Guesses so far: " + guessesSoFar.join(' ');
+    }
+      
+    countGuessesLeft();
+  
+document.onkeyup = function(event) {
+    guessesLeft--;
+      
+    var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+      
+    guessesSoFar.push(userChoice);
+    countGuessesLeft();
+    farUserChoice();       
+      
+    if (userChoice === computerChoice){
+        wins++;
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
+        restart();
+    } 
+    else if (guessesLeft === 0) {
+        losses++;
+        document.getElementById('losses').innerHTML = "Loses: " + losses;
+        restart();
+    }
+      
+ }
+  
+  
